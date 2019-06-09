2ce1e1c70b97633a9cec9032b34c81b68a4dfb55
Kaif Ahsan <kaifahsan420@gmail.com>
2019-06-09 18:03:35 +1000

    Adding testing function and fixed minor bug. Value now number instead of string.

--- app.js
+++ app.js
@@ -47,6 +47,10 @@ var budgetController = (function () {
             // Returning new item 
             return newItem;
             
+        },
+
+        testing: function() {
+            return data;
         }
     };
 
@@ -70,7 +74,7 @@ var UIController = (function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // Will be 'inc' or 'exp'
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value,
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value) 
             };
         },
 
